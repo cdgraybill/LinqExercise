f1a28d674723c60f012d117b75f4b4638b5254a7
Cameron Graybill <cdgraybill@gmail.com>
2020-02-03 19:28:04 -0600

    Add project files.

---
+++ LinqExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = new string[] { "Honda", "Toyota", "Ford", "Dodge" };
+        }
+    }
+}
